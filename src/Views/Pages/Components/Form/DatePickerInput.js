dd9bcc9dc02ea0ba5f32cb1071686944a9b824cd
miladmazaheri <48717848+miladmazaheri@users.noreply.github.com>
2024-08-11 20:15:43 +0330

    Fix DatePicker For Create And Update

--- src/Views/Pages/Components/Form/DatePickerInput.js
+++ src/Views/Pages/Components/Form/DatePickerInput.js
@@ -1,25 +1,57 @@
-
-import moment from 'moment-jalaali';
+import moment from "moment-jalaali";
 import DatePicker from "react-multi-date-picker";
 import persian from "react-date-object/calendars/persian";
 import persian_fa from "react-date-object/locales/persian_fa";
+const DatePickerInput = ({ model, id, description }) => {
+  const handleChange = (value) => {
+    model[id] =
+      value.year +
+      "/" +
+      ("0" + value.month).slice(-2) +
+      "/" +
+      ("0" + value.day).slice(-2);
+    // model[id]=value;
+  };
+  const getPersianDate = (dateStr) => {
+    let date = moment(dateStr);
+    let year = date.jYear();
+    let month = date.jMonth() + 1;
+    let day = date.jDate();
 
-const DatePickerInput=({model,id,description})=>{
+    let strDate = year + "/";
+    if (month < 10) strDate += "0" + month;
+    else strDate += month;
+    strDate += "/";
 
-    const handleChange=(value)=>{
-        model[id]=value.year+'/'+('0'+value.month).slice(-2)+'/'+('0'+value.day).slice(-2);
-        // model[id]=value;
-    }
-    return(
-        <>
-        <label style={{marginTop:"4px"}} htmlFor={id}><b>{description}:</b></label><br/>
-        <DatePicker style={{ border: "1px solid #ced4da",
-        borderRadius: "5px",padding: "10px",color: "#495057",width:"413px",
-        height:"40px",backgroundClip:'padding-box'}}
-        value={moment(model[id]+'00:00', 'jYYYY/jM/jD HH:mm')} onChange={handleChange}
-        calendar={persian} locale={persian_fa} calendarPosition="bottom-right" />
-        </>
-    )
-}
+    if (day < 10) strDate += "0" + day;
+    else strDate += day;
+    return strDate;
+  };
+  return (
+    <>
+      <label style={{ marginTop: "4px" }} htmlFor={id}>
+        <b>{description}:</b>
+      </label>
+      <br />
+      <DatePicker
+        style={{
+          border: "1px solid #ced4da",
+          borderRadius: "5px",
+          padding: "10px",
+          color: "#495057",
+          width: "413px",
+          height: "40px",
+          backgroundClip: "padding-box",
+        }}
+        // value={moment(model[id] + "00:00", "jYYYY/jM/jD HH:mm")}
+        value={getPersianDate(model[id])}
+        onChange={handleChange}
+        calendar={persian}
+        locale={persian_fa}
+        calendarPosition="bottom-right"
+      />
+    </>
+  );
+};
 
 export default DatePickerInput;
